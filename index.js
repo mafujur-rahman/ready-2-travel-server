c7533923885c6bf9007f6174e4cdf22b65dd1260
mahfuj926 <mahfujrahman.jewel@gmail.com>
2024-04-27 22:58:40 +0600

    Get single data from db by id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -42,6 +42,13 @@ async function run() {
     app.get('/spot', async (req, res) =>{
       const cursor = spotCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/spot/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result =  await spotCollection.findOne(query);
       res.send(result);
     })
 
